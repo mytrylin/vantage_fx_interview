0dd823fa6fd74435e823d5c84232c74ed48ac5bc
mytrylin <mytrylin@gmail.com>
2021-11-10 22:48:29 +0800

    [test] 1.components 2., props 3.fetch api

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,55 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <div>TEST</div>
+  <div >
+    {{dataTest}}
+    <button @click="fetchAPI">TESTBTN</button>
+    <p>{{fetchTest}}</p>
+    <vantage :msg="fetchTest + 1" :inemit="getEmitData"  @emitevent="emitAdd"></vantage>
+    <p>{{getEmitData}}</p>
+  </div>
 </template>
 
 <script>
-
+import Vantage from './components/Vantage.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    Vantage
+  },
+  data() {
+    return {
+      dataTest: 'dataTest',
+      fetchTest: {},
+      getEmitData: 0
+    }
+  },
+  computed: {
+  },
+  methods: {
+    emitAdd(val) {
+      this.getEmitData = val + val
+    },
+    fetchAPI() {
+      fetch('https://api.coingecko.com/api/v3/exchange_rates')
+        .then((res) => {
+          console.log('api res')
+          console.log(res)
+
+          this.fetchTest = res.status
+          console.log('this.fetchTest:')
+          console.log(this.fetchTest)
+        })
+    },
+    fetchAPI2() {
+      fetch('https://api.coingecko.com/api/v3/exchange_rates')
+        .then((res) => {
+          this.fetchTest = res.status - 1
+        })
+    }
+  },
+  created() {
+    this.fetchAPI2()
   }
 }
 </script>
---
+++ src/components/Vantage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div> 測試組件: {{msg}}</div>
+    <p>
+      <button @click="addData">EMITADD</button>
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Vantage',
+  props: {
+    msg: {
+      type: Number
+    },
+    inemit: {
+      type: Number
+    }
+  },
+  methods: {
+    addData() {
+      console.log(this.msg)
+      this.$emit('emitevent', this.msg + this.inemit)
+    }
+  }
+}
+</script>
